d365f2c0ca3abc13ad134c6d0ef4a0c2c1325117
Tanmay-sharma-ts <64952465+Tanmay-sharma-ts@users.noreply.github.com>
2025-05-16 16:14:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomDiceImage1 = "images/" + randomNumber1 + ".png";
+var randomDiceImage2 = "images/" + randomNumber2 + ".png";
+
+document.querySelector(".dice1").setAttribute("src",randomDiceImage1);
+document.querySelector(".dice2").setAttribute("src",randomDiceImage2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerText = "Player 1 Won!";
+}
+else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerText = "Player 2 Won!";
+}
+else {
+    document.querySelector("h1").innerText = "Draw, please refresh to play";
+}
